6c22c313607cd6863f7873d0d6cc4d72f2b78129
Sagar D <sagardg33@gmail.com>
2026-04-09 08:08:36 +0530

    Tailwinf shadCN setup complete

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-2xl bg-background text-primary">Hello EchoLayer!!</div>
+    <div className="text-2xl bg-background text-primary">
+      Hello EchoLayer!
+    </div>
   );
 }

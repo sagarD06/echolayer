f0f970394bef244d31249959c5a1dcade9190dd9
Sagar D <sagardg33@gmail.com>
2026-04-10 05:23:18 +0530

    Logo and test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="text-2xl bg-background text-primary">
       Hello EchoLayer!
+      <Image src="/EchoLayer.svg" alt="logo" width={220} height={90}/>
     </div>
   );
 }
